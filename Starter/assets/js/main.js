11c2c77c2d9adb477fe0411e225e3f5ccad6649c
Nebiyu Yohannes <nebiyuyohannes2020@gmail.com>
2023-04-24 23:40:38 +0300

    nav edit

--- Starter/assets/js/main.js
+++ Starter/assets/js/main.js
@@ -3,21 +3,41 @@
     // passing the selector to the element_selector variable
     const element_selector = document.querySelector(selector);
     if (element_selector) return element_selector;
+    console.log()
     throw new Error (`no such selector as ${selector} or check spelling`);
+
  };
 
- console.log(ElementSelector('.navbar'));
+ // console.log(ElementSelector('.navbar'));
 
 //Nav styles on scroll
 const scrollHeader = () => {
+   // creating a variable that holds the element
    const headerElement = ElementSelector('#header');
+   // based on a condition add and remove classes
    if (this.scrollY >= 15){
       headerElement.classList.add('activated');
    }else{
       headerElement.classList.remove('activated');
-   }
+       }
 };
+// we need to call the function for it work
+// by attaching and event listener to the window object for a scroll event
+window.addEventListener('scroll', scrollHeader);
+
 // Open menu & search pop-up
+const menuToggleIcon = ElementSelector('#menu-toggle-icon');
+
+const toggleMenu = () => {
+   // creating a varaible to store the menu id
+   const mobileMenu = ElementSelector('.menu');
+   console.log('hi')
+   // to toggle the classes under menu and under menu-toggle-icon
+   mobileMenu.classList.toggle('activated');
+   menuToggleIcon.classList.toggle('activated');
+};
+// call the function and add an event listener on the click event
+menuToggleIcon.addEventListener('click', toggleMenu);
 
 // Open/Close search form popup
 
